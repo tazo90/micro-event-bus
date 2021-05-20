bb6ac71da83c55f87a97d4dcba8a3fae0302edce
tazo90 <tazo90@gmail.com>
2021-05-21 00:22:27 +0200

    Add RabbitMQ connector

---
+++ src/rabbitMQConnection.ts
@@ -0,0 +1,24 @@
+import { Inject, Service } from 'typedi';
+import amqp from 'amqplib';
+
+@Service('rabbitmq')
+export default class RabbitMQConnection {
+  public connection;
+  public eventBusUrl: string;
+  private _retryCount: number;
+
+  constructor(
+    @Inject('logger') private _logger,
+    eventBusUrl: string
+  ) {
+    this.connection = this.connect();
+    this.eventBusUrl = eventBusUrl;
+  }
+
+  async connect() {
+    const connection = await amqp.connect(this.eventBusUrl);
+    this._logger.info('RabbitMQ client acquired a persistent connection to server');
+    return connection;
+  }
+
+}
