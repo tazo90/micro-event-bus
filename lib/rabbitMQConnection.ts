b0e2beff22b37003fbda6671de60f7ca61f5a1dd
tazo90 <tazo90@gmail.com>
2021-06-09 12:34:57 +0200

    Remove DI from rabbitmq connection

--- lib/rabbitMQConnection.ts
+++ lib/rabbitMQConnection.ts
@@ -1,23 +1,15 @@
-import { Inject, Service } from 'typedi';
 import amqp from 'amqplib';
 
-@Service('rabbitmq')
 export default class RabbitMQConnection {
   public connection;
   public eventBusUrl: string;
 
-  constructor(
-    @Inject('logger') private _logger,
-    eventBusUrl: string
-  ) {
-    this.connection = this.connect();
+  constructor(eventBusUrl: string) {
     this.eventBusUrl = eventBusUrl;
+    this.connection = this.connect();
   }
 
   async connect() {
-    const connection = await amqp.connect(this.eventBusUrl);
-    this._logger.info('RabbitMQ client acquired a persistent connection to server');
-    return connection;
+    return await amqp.connect(this.eventBusUrl);
   }
-
 }
